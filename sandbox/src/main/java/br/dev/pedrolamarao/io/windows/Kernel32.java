305cfed6731702f2a8c41bde339fe76ecabd5c62
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-02-23 20:31:19 -0300

    Add GetQueuedCompletionStatus and PostQueuedCompletionStatus.

--- sandbox/src/main/java/br/dev/pedrolamarao/io/windows/Kernel32.java
+++ sandbox/src/main/java/br/dev/pedrolamarao/io/windows/Kernel32.java
@@ -12,7 +12,6 @@ public final class Kernel32
 {
     public static final MemorySegment INVALID_HANDLE_VALUE = MemorySegment.ofAddress(-1);
 
-
     public static int CloseHandle (MemorySegment handle) throws Throwable
     {
         try (var arena = Arena.openConfined())
@@ -23,6 +22,7 @@ public final class Kernel32
             return result;
         }
     }
+
     public static MemorySegment CreateIoCompletionPort (MemorySegment device, MemorySegment port, MemorySegment key, int concurrency) throws Throwable
     {
         try (var arena = Arena.openConfined())
@@ -39,6 +39,28 @@ public final class Kernel32
         return LastError.get();
     }
 
+    public static int GetQueuedCompletionStatus (MemorySegment port, MemorySegment bytes, MemorySegment key, MemorySegment operation, int milliseconds) throws Throwable
+    {
+        try (var arena = Arena.openConfined())
+        {
+            final var callState = arena.allocate(CaptureLastError.layout());
+            final var result = (int) GetQueuedCompletionStatus.invokeExact(callState,port,bytes,key,operation,milliseconds);
+            LastError.set( (int) GetLastError.get(callState) );
+            return result;
+        }
+    }
+
+    public static int PostQueuedCompletionStatus (MemorySegment port, int bytes, MemorySegment key, MemorySegment operation) throws Throwable
+    {
+        try (var arena = Arena.openConfined())
+        {
+            final var callState = arena.allocate(CaptureLastError.layout());
+            final var result = (int) PostQueuedCompletionStatus.invokeExact(callState,port,bytes,key,operation);
+            LastError.set( (int) GetLastError.get(callState) );
+            return result;
+        }
+    }
+
     private static final ThreadLocal<Integer> LastError = ThreadLocal.withInitial(() -> -1);
 
     private static final CaptureCallState CaptureLastError = Linker.Option.captureCallState("GetLastError");
@@ -48,6 +70,10 @@ public final class Kernel32
     private static final MethodHandle CreateIoCompletionPort;
 
     private static final VarHandle GetLastError;
+
+    private static final MethodHandle GetQueuedCompletionStatus;
+
+    private static final MethodHandle PostQueuedCompletionStatus;
 
     static
     {
@@ -64,6 +90,16 @@ public final class Kernel32
             CaptureLastError
         );
         GetLastError = CaptureLastError.layout().varHandle(MemoryLayout.PathElement.groupElement("GetLastError"));
+        GetQueuedCompletionStatus = linker.downcallHandle(
+            kernel32.find("GetQueuedCompletionStatus").orElseThrow(),
+            FunctionDescriptor.of(JAVA_INT,ADDRESS,ADDRESS,ADDRESS,ADDRESS,JAVA_INT),
+            CaptureLastError
+        );
+        PostQueuedCompletionStatus = linker.downcallHandle(
+            kernel32.find("PostQueuedCompletionStatus").orElseThrow(),
+            FunctionDescriptor.of(JAVA_INT,ADDRESS,JAVA_INT,ADDRESS,ADDRESS),
+            CaptureLastError
+        );
     }
 
     private Kernel32() {}
--- sandbox/src/test/java/br/dev/pedrolamarao/io/windows/Kernel32Test.java
+++ sandbox/src/test/java/br/dev/pedrolamarao/io/windows/Kernel32Test.java
@@ -2,8 +2,14 @@ package br.dev.pedrolamarao.io.windows;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+
 import static br.dev.pedrolamarao.io.windows.Kernel32.INVALID_HANDLE_VALUE;
 import static java.lang.foreign.MemorySegment.NULL;
+import static java.lang.foreign.ValueLayout.ADDRESS;
+import static java.lang.foreign.ValueLayout.JAVA_INT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -25,5 +31,33 @@ public class Kernel32Test
         final var port = Kernel32.CreateIoCompletionPort(INVALID_HANDLE_VALUE,NULL,NULL,0);
         assertNotEquals( NULL, port );
         assertEquals( 1, Kernel32.CloseHandle(port) );
+    }
+
+    @Test
+    public void PostGetCompletionPort () throws Throwable
+    {
+        final var port = Kernel32.CreateIoCompletionPort(INVALID_HANDLE_VALUE,NULL,NULL,0);
+        assertNotEquals( NULL, port );
+
+        final var bytes = 1;
+        final var key = MemorySegment.ofAddress(2);
+        final var operation = MemorySegment.ofAddress(3);
+        assertEquals( 1, Kernel32.PostQueuedCompletionStatus(port,bytes,key,operation) );
+
+        try (var arena = Arena.openConfined())
+        {
+            final var bytesRef = arena.allocate(JAVA_INT);
+            final var keyRef = arena.allocate(ADDRESS);
+            final var operationRef = arena.allocate(ADDRESS);
+            assertEquals( 1, Kernel32.GetQueuedCompletionStatus(port,bytesRef,keyRef,operationRef,0) );
+
+            assertEquals( bytes, bytesRef.get(JAVA_INT,0) );
+            assertEquals( key, keyRef.get(ADDRESS,0) );
+            assertEquals( operation, operationRef.get(ADDRESS,0) );
+        }
+        finally
+        {
+            assertEquals( 1, Kernel32.CloseHandle(port) );
+        }
     }
 }
